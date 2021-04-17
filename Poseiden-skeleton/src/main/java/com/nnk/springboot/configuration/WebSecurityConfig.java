4c806c303cf6e2db32f5c355071b5333e88848b3
adrbessy <adrienbessy@hotmail.com>
2021-04-17 15:04:22 +0200

    can go to login page

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/WebSecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/WebSecurityConfig.java
@@ -1,17 +1,64 @@
 package com.nnk.springboot.configuration;
 
+import com.nnk.springboot.service.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("admin").password(passwordEncoder().encode("admin123")).roles("USER", "ADMIN");
+
+    auth.authenticationProvider(authenticationProvider());
+  }
+
+  @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
+
+    http.authorizeRequests()
+        .antMatchers("/admin").hasRole("ADMIN")
+        .antMatchers("/user").authenticated()
+        .anyRequest().permitAll()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .usernameParameter("username")
+        .defaultSuccessUrl("/user")
+        .permitAll()
+        .and().logout().logoutSuccessUrl("/").permitAll();
   }
+
+  @Override
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new CustomUserDetailsService();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
 
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -1,40 +1,48 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.repositories.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("app")
 public class LoginController {
 
-    @Autowired
-    private UserRepository userRepository;
+  private static final Logger logger = LogManager.getLogger(LoginController.class);
 
-    @GetMapping("login")
-    public ModelAndView login() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("login");
-        return mav;
-    }
+  @Autowired
+  private UserRepository userRepository;
 
-    @GetMapping("secure/article-details")
-    public ModelAndView getAllUserArticles() {
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("users", userRepository.findAll());
-        mav.setViewName("user/list");
-        return mav;
-    }
+  /**
+   * Just calls the login page
+   * 
+   * @return - The name of the html page
+   */
+  @GetMapping("/login")
+  public ModelAndView login() {
+    logger.info("Get request with the endpoint 'login'");
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("login");
+    return mav;
+  }
 
-    @GetMapping("error")
-    public ModelAndView error() {
-        ModelAndView mav = new ModelAndView();
-        String errorMessage= "You are not authorized for the requested data.";
-        mav.addObject("errorMsg", errorMessage);
-        mav.setViewName("403");
-        return mav;
-    }
+  @GetMapping("secure/article-details")
+  public ModelAndView getAllUserArticles() {
+    ModelAndView mav = new ModelAndView();
+    mav.addObject("users", userRepository.findAll());
+    mav.setViewName("user/list");
+    return mav;
+  }
+
+  @GetMapping("error")
+  public ModelAndView error() {
+    ModelAndView mav = new ModelAndView();
+    String errorMessage = "You are not authorized for the requested data.";
+    mav.addObject("errorMsg", errorMessage);
+    mav.setViewName("403");
+    return mav;
+  }
 }
